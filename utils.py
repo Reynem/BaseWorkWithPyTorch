98a1399b1e9f105b01ed42c9ebe9550a98877c15
Reynem <156691965+Reynem@users.noreply.github.com>
2024-03-16 17:43:13 +0500

    Created utils.py

---
+++ utils.py
@@ -0,0 +1,47 @@
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+def train_with_plot(model, train_loader, criterion, optimizer, num_epochs=11):
+    train_loss_hist = []
+    model.train()
+    for epoch in range(num_epochs):
+        running_loss = 0.0
+        for inputs, labels in train_loader:
+            inputs, labels = inputs.to(device), labels.to(device)
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item() * inputs.size(0)
+        epoch_loss = running_loss / len(train_loader.dataset)
+        train_loss_hist.append(epoch_loss)
+        print(f"Эпоха [{epoch + 1}/{num_epochs}], Лосс: {epoch_loss:.4f}")
+        plt.plot(np.arange(len(train_loss_hist)), train_loss_hist)
+        plt.xlabel('Эпохи')
+        plt.ylabel('Трейнинг лосс')
+        plt.title('Трейнинг лосс')
+        plt.show()
+
+    return train_loss_hist
+
+def evaluate_model(model, test_loader):
+    model.eval()
+    all_preds = []
+    all_labels = []
+    with torch.no_grad():
+        for inputs, labels in test_loader:
+            inputs, labels = inputs.to(device), labels.to(device)
+            outputs = model(inputs)
+            _, preds = torch.max(outputs, 1)
+            all_preds.extend(preds.cpu().numpy())
+            all_labels.extend(labels.cpu().numpy())
+
+    accuracy = accuracy_score(all_labels, all_preds)
+    precision = precision_score(all_labels, all_preds, average='macro')
+    recall = recall_score(all_labels, all_preds, average='macro')
+    f1 = f1_score(all_labels, all_preds, average='macro')
+
+    print(f"Accuracy: {accuracy:.4f}, Precision: {precision:.4f}, Recall: {recall:.4f}, F1-score: {f1:.4f}")
